d115990b59c2ed41c392d01a59c9ee8deb5ad10d
Aditya Arpan Sahoo <adityaarpansahoo@gmail.com>
2021-10-23 01:40:05 +0530

    updated code

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
-const hamburger = document.querySelector('.hamburger');
-const navLinks = doucment.querySelector('.nav-links');
-const links = document.querySelectorAll('.nav-links li');
+// const hamburger = document.querySelector('.hamburger');
+// const navLinks = doucment.querySelector('.nav-links');
+// const links = document.querySelectorAll('.nav-links li');
 
-hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('open');
+// hamburger.addEventListener('click', () => {
+//     navLinks.classList.toggle('open');
+// })
+
+// js for faqs
+let answers=document.querySelectorAll(".accordion");
+answers.forEach((event)=>{
+    event.addEventListener('click',()=>{
+        if(event.classList.contains("active")){
+            event.classList.remove("active");
+        }
+        else{
+            event.classList.add("active");
+        }
+    })
 })
